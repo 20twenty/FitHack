92b101f3db0c4fb90c05b1be645fad5d9038de60
20twenty <brady_benware@hotmail.com>
2013-08-18 14:04:22 -0700

    init

---
+++ src/com/liketivist/fithack/MainActivity.java
@@ -0,0 +1,36 @@
+package com.liketivist.fithack;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+   private Button _theButton;
+   
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      _theButton = (Button)this.findViewById(R.id.theButton);
+      _theButton.setOnClickListener(this);
+   }
+
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      getMenuInflater().inflate(R.menu.main, menu);
+      return true;
+   }
+
+   @Override
+   public void onClick(View v) {
+      if ( v == _theButton ) {
+         Toast.makeText(this, "You pressed THE BUTTON", Toast.LENGTH_LONG).show();
+      }
+   }
+
+}
---
+++ src/com/liketivist/fithack/MapMyRunQuery.java
@@ -0,0 +1,8 @@
+package com.liketivist.fithack;
+
+public class MapMyRunQuery {
+   
+   public void getRoute(float distance, float latitude, float longitude) {
+      
+   }
+}
