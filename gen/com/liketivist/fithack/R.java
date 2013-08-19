9e915dae093f8ccd6468bf761154cac7dc7f27c8
20twenty <brady_benware@hotmail.com>
2013-08-18 18:05:36 -0700

    x

--- gen/com/liketivist/fithack/R.java
+++ gen/com/liketivist/fithack/R.java
@@ -10,6 +10,16 @@ package com.liketivist.fithack;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int babyblue=0x7f040004;
+        public static final int blue=0x7f040000;
+        public static final int dark_gray=0x7f040007;
+        public static final int green=0x7f040001;
+        public static final int lightbabyblue=0x7f040005;
+        public static final int orangy=0x7f040006;
+        public static final int red=0x7f040002;
+        public static final int white=0x7f040003;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,34 +27,40 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int button_border=0x7f050002;
+        public static final int button_text_size=0x7f050004;
+        public static final int button_width=0x7f050005;
+        public static final int corner_radius=0x7f050003;
     }
     public static final class drawable {
         public static final int button=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int editText1=0x7f080000;
-        public static final int editText2=0x7f080001;
-        public static final int editText3=0x7f080002;
-        public static final int theButton=0x7f080003;
+        public static final int action_settings=0x7f090005;
+        public static final int editText1=0x7f090000;
+        public static final int editText2=0x7f090001;
+        public static final int editText3=0x7f090002;
+        public static final int editText4=0x7f090004;
+        public static final int theButton=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int appname=0x7f050003;
-        public static final int buttontext=0x7f050006;
-        public static final int description=0x7f050005;
-        public static final int hello_world=0x7f050002;
-        public static final int tagline=0x7f050004;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int appname=0x7f060003;
+        public static final int buttontext=0x7f060006;
+        public static final int description=0x7f060005;
+        public static final int hello_world=0x7f060002;
+        public static final int poweredby=0x7f060007;
+        public static final int tagline=0x7f060004;
     }
     public static final class style {
         /** 
@@ -68,10 +84,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
